ff7cac9ec44d4924458a660f6397aade4e11bd97
Sergey <konsaltpns@gmail.com>
2025-02-18 21:08:55 +0200

    hjk

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ document.querySelector('.sidenav').addEventListener('click', (event) => {
     event.preventDefault(); // предотвращаем переход по ссылке
     try {
         if (event.target.closest('.t1')) {
-            loadContent('/partials/lecture-1.html', '.m1');
+            loadContent('./lecture-1.html', '.m1');
         }
         else if (event.target.closest('.t2')) {
             document.querySelector('.m1').innerHTML = '';
-            loadContent('/partials/lecture-2.html', '.m1');
+            loadContent('./partials/lecture-2.html', '.m1');
         }
     }
-    catch {
-        console.log("Error");        
+    catch (error){
+        console.log("Error",error);        
     }
 });
 
