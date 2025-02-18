52b61f2120ed2fa6b6a969621a6a7a3f101d03b6
Sergey <konsaltpns@gmail.com>
2025-02-18 20:58:11 +0200

    jjjjj

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import { loadContent } from './JS/first.js';
 
 document.querySelector('.sidenav').addEventListener('click', (event) => {
     event.preventDefault(); // предотвращаем переход по ссылке
-    if (event.target.closest('.t1')) {        
-        loadContent('/partials/lecture-1.html', '.m1');alert(';;;');
+    try {
+        if (event.target.closest('.t1')) {
+            loadContent('/partials/lecture-1.html', '.m1');
+        }
+        else if (event.target.closest('.t2')) {
+            document.querySelector('.m1').innerHTML = '';
+            loadContent('/partials/lecture-2.html', '.m1');
+        }
     }
-    else if (event.target.closest('.t2')) {
-        document.querySelector('.m1').innerHTML = '';
-        loadContent('/partials/lecture-2.html', '.m1');        
+    catch {
+        console.log("Error");
+        
     }
 });
 
