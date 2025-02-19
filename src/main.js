acc2d21aa4dfbc7092aba08a18285551fd3d9717
Sergey <konsaltpns@gmail.com>
2025-02-19 10:09:41 +0200

    l

--- src/main.js
+++ src/main.js
@@ -1,12 +1,25 @@
 
-import { loadContent } from '/JS/first.js';
+//import { loadContent } from '/JS/first.js';
+async function loadContent(url, containerId) {
+    try {
+        alert('&&&');
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Ошибка загрузки: ${response.status}`);
+        const html = await response.text();
+        document.querySelector(containerId).innerHTML = html;
+    } catch (error) {
+        console.error("Ошибка при загрузке контента:", error);
+    }
+}
+
+
 
 document.querySelector('.sidenav').addEventListener('click', (event) => {
     event.preventDefault(); // предотвращаем переход по ссылке
     try {
         if (event.target.closest('.t1')) {
             // loadContent('./partials/lecture-1.html', '.m1');
-            loadContent('/lecture-1.html', '.m1');
+            loadContent('/src/partials/lecture-1.html', '.m1');
         }
         else if (event.target.closest('.t2')) {
             document.querySelector('.m1').innerHTML = '';
