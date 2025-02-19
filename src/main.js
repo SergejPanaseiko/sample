c2af6ee97073815f9e5852feb4b78281d8a35a7f
Sergey <konsaltpns@gmail.com>
2025-02-19 11:56:16 +0200

    gjhgjh

--- src/main.js
+++ src/main.js
@@ -1,33 +1,19 @@
 
-//import { loadContent } from '/JS/first.js';
-async function loadContent(url, containerId) {
-    try {
-       
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(`Ошибка загрузки: ${response.status}`);
-        const html = await response.text();
-        document.querySelector(containerId).innerHTML = html;
-    } catch (error) {
-        console.error("Ошибка при загрузке контента:", error);
-    }
-}
+import { loadContent } from '/JS/first.js';
 
 document.querySelector('.sidenav').addEventListener('click', (event) => {
     event.preventDefault(); // предотвращаем переход по ссылке
     try {
         if (event.target.closest('.t1')) {
-            alert('&&&');
-            document.querySelector('.m1').innerHTML = '';
-            loadContent('./partials/lecture-1.html', '.m1');
-            //loadContent('./lecture-1.html', '.m1');
+            // loadContent('./partials/lecture-1.html', '.m1');
+            loadContent('/lecture-1.html', '.m1');
         }
         else if (event.target.closest('.t2')) {
             document.querySelector('.m1').innerHTML = '';
-            loadContent('./lecture-2.html', '.m1');
+            loadContent('/lecture-2.html', '.m1');
         }
     }
     catch (error){
         console.log("Error",error);        
     }
 });
-
